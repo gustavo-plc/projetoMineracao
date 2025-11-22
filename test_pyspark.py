693f245ff510a5d71d04c8ae9b0f4e2100120495
Gustavo Cunha <gustavoparreira@hotmail.com>
2025-11-22 18:47:48 -0300

    alterações para java 17 e python 11

--- test_pyspark.py
+++ test_pyspark.py
@@ -1,4 +1,12 @@
+import sys
+import os
 from pyspark.sql import SparkSession
+
+# --- ADICIONE ESTAS DUAS LINHAS ---
+# Elas forçam o Spark a usar o mesmo Python que está rodando o script (o do venv)
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+# ----------------------------------
 
 spark = SparkSession.builder.appName("Test").getOrCreate()
 
